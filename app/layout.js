4264dea375237214377befb0c72185a821fb65dc
bayefalldjembefoundation-ship-it <bayefalldjembefoundation@gmail.com>
2026-09-08 23:16:54 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+</> JavaScript
+export const metadata = {
+  title: "Baye Fall Djembe Foundation",
+  description:
+    "Connecting communities through West African culture, music, education, and heritage.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
