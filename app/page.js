caec339f85f13b6903b53b7818a77a2d0345a4f2
bayefalldjembefoundation-ship-it <bayefalldjembefoundation@gmail.com>
2026-09-08 23:13:45 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,106 @@
+</>  JavaScript
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <h1>Baye Fall Djembe Foundation</h1>
+
+        <p>
+          Connecting communities through West African cultures, music,
+          education,and heritage.
+        </p>
+
+        <button>Support Our Mission</button>
+      </section>
+
+      <section>
+        <h2>Our Mission</h2>
+
+        <p>
+          Our mission is to build bridges between the United States and 
+          West africa through culture, music, education and community.
+          We empower youth to discover their roots, celebrate their heritage,
+          and create lasting connections across different generations, cultures,
+          and borders.
+        </p>
+     </section>
+
+     <section>
+        <h2>Our Programs</h2>
+            
+        <div>
+          <h3>Rhthym & Movement</h3>
+          <p>
+            Experience the heartbeat of West Africa through traditional
+            rhthyms, energetic dance, and powerful storytelling. Participants
+            learn the cultural meaning behind the rhythms, explore traditional
+            drum-making techniques, and discover how music, movement, and stories
+            connect generations.
+          </p>
+        </div>
+
+        <div>
+          <h3>Youth Programs</h3>
+          <p>
+            Bring the sounds, movement, and traditions of West Africa directly
+            to young people through engaging school programs and dedicated summer
+            camps. Youth experience West African drumming, dance, storytelling,
+            and cultural education while building confidence, creativity, and a
+            deeper connection to culture and community.
+          </p>
+        </div>
+
+        <div>
+          <h3>Bridging Two World</h3>
+          <p>
+            Experience Senegal through an unforgettable cultural journey designed
+            to connect you with the heart of West Africa. From music, dance, food,
+            and traditions to meaningful community experiences, the Baye Fall Djembe
+            Foundation creates opportunities to explore, connect, and experience Senegal
+            in a way that leaves a lasting impact.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>Get Involved</h2>
+
+        <h3>Be part of something bigger!</h3>
+
+        <p>
+          Whether you want to support our programs, bring West African culture
+          to your school or community, volunteer, partner with us, or experience
+          Senegal with us, there’s a place for you at the Baye Fall Djembe Foundation.
+        </p>
+
+        <div>
+          <div>
+            <h3>Partner With Us</h3>
+            <p>
+              Schools, organizations, and community partners can work with us
+              to bring cultural education and programming to their communities.
+            </p>
+          </div>
+
+          <div>
+            <h3>Bring Culture to Your School</h3>
+            <p>
+              Book our drum, dance, and storytelling programs for your school,
+              organization, or community.
+            </p>
+          </div>
+
+           <div>
+              <h3>Have questions or want to get involved?</h3>
+
+              <p>Contact us:</p>
+
+              <p>📧 Email: [bayefalldjembefoundation@gmail.com]</p>
+              <p>📞 Phone: [+1-651-334-0769]</p>
+              <p>📍Minnesota</p>
+            </div>
+        </section>
+       </main>
+    );
+}
+              
